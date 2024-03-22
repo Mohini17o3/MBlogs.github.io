b7c90f8ab8646e706cdff783037a284b5b1d44d4
Mohini <upretimohini@gmail.com>
2024-03-22 23:28:31 +0530

    writing first blog and styling

---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express" ;
+import bodyParser from "body-parser";
+import axios from "axios";
+const app = express();
+const port = 3000 ;
+app.set('view engine', 'ejs');
+
+ 
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/" , (req , res)=>{
+    res.render("index.ejs");
+})
+
+app.get("/home" , (req,res)=>{
+    res.render('index.ejs');
+})
+
+app.get("/about" , (req , res)=>{
+    res.render("about.ejs");
+})
+
+app.get("/contact" , async(req , res)=>{
+try{
+    const response = await axios.get('https://type.fit/api/quotes');
+    const quotes =  response.data.map(quote=>quote.text) ;
+    const randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    res.render('contact.ejs' , {randomQuote});
+} catch(error){
+    console.error('Error fetching data:' , error);
+    res.status(500).send('Error fetching data');
+}
+
+});
+
+app.get("/card1" ,(req , res)=>{
+     res.render("card1.ejs");
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
+  // in main body we want card to dispaly image and title of the blog  
+  // but what about add new functionality , we need to figure that out too 
