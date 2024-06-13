49a6c40743c580dfb81238a95e0414c114cc2009
Mohini <upretimohini@gmail.com>
2024-06-13 18:33:23 +0530

    temp style changes

--- public/script.js
+++ public/script.js
@@ -2,8 +2,44 @@
   window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     if (window.scrollY > 50) { // Adjust the scroll distance as needed
-        header.style.opacity = '0.5'; // Set to desired opacity
+        header.style.opacity = '0.2'; // Set to desired opacity
     } else {
         header.style.opacity = '1';
     }
 });
+
+
+//Dark mode for body 
+
+
+const toggleBtn = document.getElementById("toggle-btn");
+const theme = document.body;
+let darkMode = localStorage.getItem("dark-mode");
+
+
+const enableDarkMode = () => {
+    theme.classList.add("dark-mode-theme");
+    localStorage.setItem("dark-mode" , "enabled");
+
+};
+
+const disableDarkMode = () =>{
+    theme.classList.remove("dark-mode-theme");
+    localStorage.setItem("dark-mode" , "disabled");
+
+};
+
+if(darkMode === "enabled"){
+    enableDarkMode();
+}
+
+toggleBtn.addEventListener("click" , () =>{
+    darkMode = localStorage.getItem("dark-mode");
+    if(darkMode === "disabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
+
+
